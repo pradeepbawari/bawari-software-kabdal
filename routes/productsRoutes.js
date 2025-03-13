eecd91752c7ec183373f00b64e6a2ed28b265758
pradeep <pradeepbawari@gmail.com>
2025-03-13 06:16:02 +0530

    Updated controllers, models, and routes

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,11 +1,12 @@
 // routes/productsController.js
 const express = require("express");
 const router = express.Router();
-const { createProducts, getAllProducts, updateProducts, deleteProducts, filterProducts, filterUserProducts } = require("../controllers/productsController");
+const { createProducts, getAllProducts, updateProducts, deleteProducts, filterProducts, filterUserProducts, fetchSingleProduct } = require("../controllers/productsController");
 // const { getAllProducts} = require("../controllers/productsControllerNew");
 
 router.post("/create", createProducts);
 router.post("/all", getAllProducts);
+router.post("/productbyid", fetchSingleProduct);
 router.put("/update/:id", updateProducts);
 router.delete("/delete/:id", deleteProducts);
 router.post("/filterProducts", filterProducts);
