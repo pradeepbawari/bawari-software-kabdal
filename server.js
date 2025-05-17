44cf770650816dfafbc44828eea0c904d43ccec5
pradeep <pradeepbawari@gmail.com>
2025-05-17 18:01:01 +0530

    'https'

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const corsOptions = {
   // local
   // origin: ['http://gobuyly.com', 'http://admin.gobuyly.com', 'http://147.93.28.231', 'http://srv748278.hstgr.cloud', 'http://localhost:5173', 'http://localhost:5174'], 
   // production
-   origin: ['https://admin.gobuyly.com', 'https://147.93.28.231', 'http://srv748278.hstgr.cloud', 'https://srv748278.hstgr.cloud', 'https://gobuyly.com'], 
+   origin: ['http://admin.gobuyly.com', 'http://147.93.28.231', 'http://srv748278.hstgr.cloud', 'https://gobuyly.com'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Required if using cookies or tokens
