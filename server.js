0d5a2ca26daeb2e086d77186c38b96ab3b062d9c
pradeep <pradeepbawari@gmail.com>
2025-05-17 17:38:03 +0530

    https issue

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ const app = express();
 
 const corsOptions = {
   // local
-  //origin: ['http://gobuyly.com', 'http://admin.gobuyly.com', 'http://147.93.28.231', 'http://srv748278.hstgr.cloud', 'http://localhost:5173', 'http://localhost:5174'], 
+  // origin: ['http://gobuyly.com', 'http://admin.gobuyly.com', 'http://147.93.28.231', 'http://srv748278.hstgr.cloud', 'http://localhost:5173', 'http://localhost:5174'], 
   // production
-   origin: ['http://gobuyly.com', 'http://admin.gobuyly.com', 'http://147.93.28.231', 'http://srv748278.hstgr.cloud', 'https://gobuyly.com', 'https://admin.gobuyly.com', 'https://147.93.28.231', 'https://srv748278.hstgr.cloud'], 
+   origin: ['https://admin.gobuyly.com', 'https://147.93.28.231', 'http://srv748278.hstgr.cloud', 'https://srv748278.hstgr.cloud', 'https://gobuyly.com'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Required if using cookies or tokens
@@ -46,7 +46,7 @@ app.options('*', cors(corsOptions)); // Handle preflight requests
  const PORT = process.env.PORT || 5000;
 
 // Set the port local
-//const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
